6512bf7a153f753beae28ec6d9bcef7b6b4ebf63
daisuke0131 <dai.tachikoma@gmail.com>
2023-12-07 15:24:04 +0900

    fix todo_creator_test.go

--- 2023/Daisuke/backend/internal/domain/service/todo_creator_test.go
+++ 2023/Daisuke/backend/internal/domain/service/todo_creator_test.go
@@ -34,8 +34,9 @@ func TestCreateTodo(t *testing.T) {
 				f.mockTodoCommandsGateway = &gateway.TodoCommandsGatewayMock{
 					CreateTodoFunc: func(ctx context.Context, newTodo *todo.NewTodo) (*todo.Todo, error) {
 						return &todo.Todo{
-							ID:   "todo_id_1",
-							Text: "todo_text_1",
+							ID:     "todo_id_1",
+							Text:   "todo_text_1",
+							UserID: 12345,
 						}, nil
 					},
 				}
@@ -45,8 +46,9 @@ func TestCreateTodo(t *testing.T) {
 			},
 			expected: expected{
 				todo: &output.TodoCreator{
-					ID:   "todo_id_1",
-					Text: "todo_text_1",
+					ID:     "todo_id_1",
+					Text:   "todo_text_1",
+					UserID: 12345,
 				},
 			},
 			wantErr: false,
