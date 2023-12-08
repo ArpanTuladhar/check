8f5e50ed43e0997443cb3c4b4da5b857aa76c74e
GOPI B <gopi.b@1-2-364.local>
2023-12-08 11:14:10 +0900

    fix paths for standard

--- 2023/Gopi/backend/internal/handler/graph/schema.resolvers.go
+++ 2023/Gopi/backend/internal/handler/graph/schema.resolvers.go
@@ -8,7 +8,8 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/88labs/andpad-engineer-training/2023/Gopi/backend/graph/model"
+	graph "github.com/88labs/andpad-engineer-training/2023/Gopi/backend/internal/handler/graph/generated"
+	"github.com/88labs/andpad-engineer-training/2023/Gopi/backend/internal/handler/graph/model"
 )
 
 // CreateTodo is the resolver for the createTodo field.
@@ -21,11 +22,11 @@ func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
 	panic(fmt.Errorf("not implemented: Todos - todos"))
 }
 
-// Mutation returns MutationResolver implementation.
-func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
+// Mutation returns graph.MutationResolver implementation.
+func (r *Resolver) Mutation() graph.MutationResolver { return &mutationResolver{r} }
 
-// Query returns QueryResolver implementation.
-func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
+// Query returns graph.QueryResolver implementation.
+func (r *Resolver) Query() graph.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
