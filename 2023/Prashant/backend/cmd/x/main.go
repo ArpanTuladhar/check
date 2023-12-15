b398b6a1df3aab57fd3944c788c162f8cdeabc7f
Prashant Singh <prashant.singh@1-2-363.local>
2023-12-15 11:37:15 +0900

    fix:gracefully-server-shutdown

--- 2023/Prashant/backend/cmd/x/main.go
+++ 2023/Prashant/backend/cmd/x/main.go
@@ -1,29 +1,76 @@
 package main
 
 import (
-	"github.com/88labs/andpad-engineer-training/2023/Prashant/backend/internal/handler/graph"
-	generated "github.com/88labs/andpad-engineer-training/2023/Prashant/backend/internal/handler/graph/generated"
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"log"
-	"net/http"
-	"os"
+
+	"github.com/88labs/andpad-engineer-training/2023/Prashant/backend/internal/handler/graph"
+	graph "github.com/88labs/andpad-engineer-training/2023/Prashant/backend/internal/handler/graph/generated"
 )
 
-const defaultPort = "8080"
-
 func main() {
+	// context that listens for interrupt and terminate signals.
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	//  default port and read from the environment variable.
+	const defaultPort = "8080"
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
+	// GraphQL server with generated schema and resolver.
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	// Start the HTTP server in a goroutine.
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: http.DefaultServeMux,
+	}
+
+	go func() {
+		fmt.Printf("Server is starting on http://localhost:%s\n", port)
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			fmt.Printf("Server error: %v\n", err)
+			cancel()
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shut down the server.
+	waitForInterrupt(ctx, server)
+}
+
+func waitForInterrupt(ctx context.Context, server *http.Server) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	// Blocked until a signal is received or the context is canceled.
+	select {
+	case sig := <-c:
+		fmt.Printf("\nReceived signal: %v\n", sig)
+	case <-ctx.Done():
+		fmt.Println("Context canceled")
+	}
+
+	// context with a timeout for graceful shutdown.
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// shut down the server gracefully.
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		fmt.Printf("Error during server shutdown: %v\n", err)
+	} else {
+		fmt.Println("Server gracefully shut down")
+	}
 }
